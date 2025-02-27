0eb30d1143ca3cf65e97bdb57bab521eaf073820
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-02-27 19:23:20 +0530

    added custom user mapper

--- src/main/java/com/example/spendiq/mapper/UserMapper.java
+++ src/main/java/com/example/spendiq/mapper/UserMapper.java
@@ -2,11 +2,22 @@ package com.example.spendiq.mapper;
 
 import com.example.spendiq.dto.user.UserRequestDTO;
 import com.example.spendiq.dto.user.UserResponseDTO;
+import com.example.spendiq.entity.Role;
 import com.example.spendiq.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "roles", source = "user", qualifiedByName = "mapRoles")
     UserResponseDTO toUserResponseDTO(User user);
     User toUser(UserRequestDTO requestDTO);
+
+    @Named(value = "mapRoles")
+    default List<String> qualifier(User user) {
+        return user.getRoles().stream().map(Role::getRoleName).toList();
+    }
 }

089002386b0d1c8d3ee274eb22f9c9a0032a170d
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 11:54:41 +0530

    Added AccountResponseDto, Exceptions

---
+++ src/main/java/com/example/spendiq/dto/account/AccountResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.spendiq.dto.account;
+
+import com.example.spendiq.enums.AccountType;
+import com.example.spendiq.enums.Currency;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AddAccountResponseDTO {
+    private UUID accountId;
+    private String accountName;
+    private AccountType accountType;
+    private Boolean isDefault;
+    private double amount;
+    private Currency currency;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/example/spendiq/exception/AccountAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.spendiq.exception;
+
+public class AccountAlreadyExistsException extends RuntimeException {
+  public AccountAlreadyExistsException(String message) {
+    super(message);
+  }
+}

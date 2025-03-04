822050671a48ed6bd28486930016ad00890c450c
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 12:02:50 +0530

    added account repo layer

--- src/main/java/com/example/spendiq/repository/AccountRepository.java
+++ src/main/java/com/example/spendiq/repository/AccountRepository.java
@@ -1,9 +1,23 @@
 package com.example.spendiq.repository;
 
 import com.example.spendiq.entity.Account;
+import com.example.spendiq.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Account a SET a.isDefault = false WHERE a.isDefault = true AND a.user.userId = :userId")
+    void checkAndUnsetDefaultAccount(@Param("userId") UUID userId);
+
+    Optional<List<Account>> findAllByUserUserId(UUID userId);
+    boolean existsByAccountNameAndUser(String accountName, User user);
 }

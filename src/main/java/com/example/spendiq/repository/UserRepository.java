ce4c387ebca441645a02d6ae04d4ab3012a1d559
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 12:01:47 +0530

    changed datatype of id from string to uuid in user repo

--- src/main/java/com/example/spendiq/repository/UserRepository.java
+++ src/main/java/com/example/spendiq/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Component
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByEmail(String email);
 }

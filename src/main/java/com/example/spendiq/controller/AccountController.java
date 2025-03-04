b48a43cdc05cce5aa569f956f174b593b3e92405
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 12:02:34 +0530

    added account controller for account rest api's

--- src/main/java/com/example/spendiq/controller/AccountController.java
+++ src/main/java/com/example/spendiq/controller/AccountController.java
@@ -2,31 +2,76 @@ package com.example.spendiq.controller;
 
 import com.example.spendiq.api.response.StatusOk;
 import com.example.spendiq.dto.account.AddAccountRequestDTO;
+import com.example.spendiq.dto.account.AccountResponseDTO;
+import com.example.spendiq.dto.account.GetAllAccountResponseDTO;
+import com.example.spendiq.dto.account.UpdateAccountRequestDTO;
 import com.example.spendiq.entity.Account;
+import com.example.spendiq.entity.User;
 import com.example.spendiq.mapper.AccountMapper;
+import com.example.spendiq.services.AccountService;
+import com.example.spendiq.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
 
     private final AccountMapper accountMapper;
+    private final UserService userService;
+    private final AccountService accountService;
 
     @Autowired
-    public AccountController(AccountMapper accountMapper) {
+    public AccountController(AccountMapper accountMapper, UserService userService, AccountService accountService) {
         this.accountMapper = accountMapper;
+        this.userService = userService;
+        this.accountService = accountService;
     }
 
 
     @PostMapping
-    ResponseEntity<?> addAccount(@RequestBody AddAccountRequestDTO requestDTO) {
+    ResponseEntity<StatusOk<AccountResponseDTO>> addAccount(@RequestBody @Valid AddAccountRequestDTO requestDTO) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
         Account account = accountMapper.toAccount(requestDTO);
-        return new ResponseEntity<>(new StatusOk<>(account), HttpStatus.OK);
+        Account addedAccount = accountService.addAccount(account, user);
+        AccountResponseDTO responseData = accountMapper.toAccountResponseDTO(addedAccount);
+        return new ResponseEntity<>(new StatusOk<>(responseData), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    ResponseEntity<StatusOk<GetAllAccountResponseDTO>> getAllAccount() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        List<Account> accounts = accountService.getAllAccount(user.getUserId());
+        List<AccountResponseDTO> accountResponseDTOList = accountMapper.toListOfAccountResponseDTO(accounts);
+        GetAllAccountResponseDTO response = new GetAllAccountResponseDTO(accountResponseDTOList);
+        return new ResponseEntity<>(new StatusOk<>(response),HttpStatus.OK);
+    }
+
+    @GetMapping("/{accountId}")
+    ResponseEntity<StatusOk<AccountResponseDTO>> getAccountByAccountId(@PathVariable String accountId) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        Account account = accountService.findAccountById(UUID.fromString(accountId), user.getUserId());
+        AccountResponseDTO response = accountMapper.toAccountResponseDTO(account);
+        return new ResponseEntity<>(new StatusOk<>(response),HttpStatus.OK);
+    }
+
+    @PutMapping("/{accountId}")
+    ResponseEntity<StatusOk<AccountResponseDTO>> updateAccountByAccountId(@PathVariable String accountId, @RequestBody UpdateAccountRequestDTO requestData) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        Account account = accountService.updateAccount(UUID.fromString(accountId), requestData, user);
+        AccountResponseDTO response = accountMapper.toAccountResponseDTO(account);
+        return new ResponseEntity<>(new StatusOk<>(response), HttpStatus.OK);
     }
 }

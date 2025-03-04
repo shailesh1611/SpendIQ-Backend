44d429eca75759db09fb9df13376db9dd418142f
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 11:58:34 +0530

    added account mapper methods for account  dto's conversion

--- src/main/java/com/example/spendiq/mapper/AccountMapper.java
+++ src/main/java/com/example/spendiq/mapper/AccountMapper.java
@@ -1,8 +1,15 @@
 package com.example.spendiq.mapper;
 
+import com.example.spendiq.dto.account.AccountResponseDTO;
 import com.example.spendiq.dto.account.AddAccountRequestDTO;
 import com.example.spendiq.entity.Account;
+import org.mapstruct.Mapper;
 
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface AccountMapper {
     Account toAccount(AddAccountRequestDTO requestDTO);
+    AccountResponseDTO toAccountResponseDTO(Account account);
+    List<AccountResponseDTO> toListOfAccountResponseDTO(List<Account> accounts);
 }

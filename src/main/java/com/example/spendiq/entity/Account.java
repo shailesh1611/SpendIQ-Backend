01b37e0382ae861775e291a21d00eab4c90cd642
Shailesh Singh <shaileshsingh23423@gmail.com>
2025-03-04 12:01:05 +0530

    added account entity

--- src/main/java/com/example/spendiq/entity/Account.java
+++ src/main/java/com/example/spendiq/entity/Account.java
@@ -3,6 +3,9 @@ package com.example.spendiq.entity;
 import com.example.spendiq.enums.AccountType;
 import com.example.spendiq.enums.Currency;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +13,9 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "accounts")
+@Table(name = "accounts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"accountName","userId"})
+})
 @Getter
 @Setter
 public class Account {
@@ -18,13 +23,18 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID accountId;
 
+    @NotBlank(message = "Account Name is required")
+    @Column(nullable = false)
     private String accountName;
 
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
     private LocalDateTime createdAt;
-    private boolean isDefault;
+    private Boolean isDefault;
+
+    @NotNull(message = "Amount is Required")
+    @Positive(message = "Amount must be greater than 0")
     private double amount;
 
     @Enumerated(EnumType.STRING)
